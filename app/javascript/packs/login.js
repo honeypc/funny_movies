b688c29361722bd6df0de58aa197b46270e7377e
Phuoc Tu <tuquangphuoc@gmail.com>
2019-05-27 14:17:52 +0700

    Update login js

--- app/javascript/packs/login.js
+++ app/javascript/packs/login.js
@@ -4,10 +4,7 @@ import toastr from 'toastr';
 $(document).on('ajax:complete', '#new_user', function(e){
   var response = e.detail;
   var xhr = response[0], status = response[1];
-  if (xhr.statusText === "Created") {
-    user = xhr.responseJSON;
-    toastr.success('Hello ' + user.email);
-  } else {
+  if (xhr.status != 200) {
     toastr.error('Invalid email or password!');
   }
 });
